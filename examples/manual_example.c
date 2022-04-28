969ee63c1aa209a60ddb0f6e6f5ac33d454af947
Quim <quim@airmail.cc>
2022-04-28 12:01:40 +0200

    Update manual-example to use the new API

--- examples/manual_example.c
+++ examples/manual_example.c
@@ -24,116 +24,43 @@
 #include <string.h>
 #include "include/wfa_gpu.h"
 
-void fill_sequences (char* sequences_buffer, sequence_pair_t* sequence_metadata) {
-    // Copy all sequence pairs to the sequences_buffer, with a nullbyte
-    // separating the sequences (automatically added by strcpy in this case).
-    // **All sequences need to be 32-bit aligned.**
-    //
-    // The buffer should look like:
-    // QUERY1\0TEXT1\0QUERY2\0TEXT2\0QUERY3\0TEXT3\0 ...
-    //
-    // Add the sequences information in the sequence_metadata array.
-    // Pattern = query, Text = text
-
-    // Alignment 1
-    int pattern_offset = 0;
-    strcpy(
-        sequences_buffer,
-        "CCTAACCCTAACCCTAACCCTAAACCCTAAACCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCTAACCCCAACCCTAACCCCAACCCCACCTATACCCTAACCGCACCCCAACCCAAACCCCAA");
-    sequence_metadata[0].pattern_offset = 0;
-    sequence_metadata[0].pattern_len = strlen(sequences_buffer);
-    int text_offset = WFA_ALIGN_32_BITS(pattern_offset + sequence_metadata[0].pattern_len + 1);
-    strcpy(
-        sequences_buffer + text_offset,
-        "CCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAAACCCTAAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCCCAACCCTAACCCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCC");
-    sequence_metadata[0].text_offset = text_offset; // +1 for the nullbyte
-    sequence_metadata[0].text_len = strlen(sequences_buffer + sequence_metadata[0].text_offset);
-
-    // Alignment 2
-    pattern_offset = WFA_ALIGN_32_BITS(sequence_metadata[0].text_offset + sequence_metadata[0].text_len + 1);
-    strcpy(
-        sequences_buffer + pattern_offset,
-        "TAACCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAACCTAACCCTACCCTAACCCAAACCCTAACCCCAACCCCAACCCCAACCC");
-    sequence_metadata[1].pattern_offset = pattern_offset;
-    sequence_metadata[1].pattern_len = strlen(sequences_buffer + pattern_offset);
-
-    text_offset = WFA_ALIGN_32_BITS(pattern_offset + sequence_metadata[1].pattern_len + 1);
-    strcpy(
-        sequences_buffer + text_offset,
-        "TAACCCTACCCTAACCCTAACCCTACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTTAACCCTAACCCTTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCC");
-    sequence_metadata[1].text_offset = text_offset;
-    sequence_metadata[1].text_len = strlen(sequences_buffer + text_offset);
-
-    // Alignment 3
-    pattern_offset = WFA_ALIGN_32_BITS(sequence_metadata[1].text_offset + sequence_metadata[1].text_len + 1);
-    strcpy(
-        sequences_buffer + pattern_offset,
-        "GGTGAGGGTGAGGGTTAGGGTTAGGGTGAGGGTTAGGGTGAGGGTGAGGGTGAGGGTGAGGGTAGGGGTAGGGGGTAGGGGTGGGGGTGGGTGAGGGGTAGGGGTAGGGAGAGGGGTAGGGTTAGGGGTGGGGGGAGGGTGAGGGTTA");
-    sequence_metadata[2].pattern_offset = pattern_offset;
-    sequence_metadata[2].pattern_len = strlen(sequences_buffer + pattern_offset);
-
-    text_offset = WFA_ALIGN_32_BITS(pattern_offset + sequence_metadata[2].pattern_len + 1);
-    strcpy(
-        sequences_buffer + text_offset,
-        "GGTGAGGGTGAGGGTTAGGGTTAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTTAGGGTGTTAGAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGGGTTAGGGGGTTAGGGGGTTAGGGGGTTAGGGTGAGGGTGAGGGT");
-    sequence_metadata[2].text_offset = text_offset;
-    sequence_metadata[2].text_len = strlen(sequences_buffer + text_offset);
-
-    // Alignment 4
-    pattern_offset = WFA_ALIGN_32_BITS(sequence_metadata[2].text_offset + sequence_metadata[2].text_len + 1);
-    strcpy(
-        sequences_buffer + pattern_offset,
-        "CCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCTAACCCTCACCTAACCCCAACCCTAACCCCAACCCCAACCCACACCCTAC");
-    sequence_metadata[3].pattern_offset = pattern_offset;
-    sequence_metadata[3].pattern_len = strlen(sequences_buffer + pattern_offset);
-
-    text_offset = WFA_ALIGN_32_BITS(pattern_offset + sequence_metadata[3].pattern_len + 1);
-    strcpy(
-        sequences_buffer + text_offset,
-        "CCTAACCCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAAACCCTAAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCCCAACCCTAACCCCTAACCCTAACCCTAACCCTAC");
-    sequence_metadata[3].text_offset = text_offset;
-    sequence_metadata[3].text_len = strlen(sequences_buffer + text_offset);
-}
-
 int main() {
-    const int num_alignments = 4;
-    const int batch_size = 2;
+    // Create and initialize the aligner structure
+    wfagpu_aligner_t aligner = {0};
+    wfagpu_initialize_aligner(&aligner);
 
-    // Initialize sequences buffer and metadata structures
-    // Allocate 2KiB to store the sequences
-    const int sequences_buffer_size = 2048;
-    char* sequences_buffer = (char*)calloc(sequences_buffer_size, 1);
-    // Allocate space for 4 alignments
-    sequence_pair_t* sequences_metadata = (sequence_pair_t*)calloc(num_alignments, sizeof(sequence_pair_t));
-    fill_sequences(sequences_buffer, sequences_metadata);
+    // Add the sequences to align: wfagpu_add_sequences(aligner, query, target)
+    wfagpu_add_sequences(&aligner,
+                  "CCTAACCCTAACCCTAACCCTAAACCCTAAACCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCTAACCCCAACCCTAACCCCAACCCCACCTATACCCTAACCGCACCCCAACCCAAACCCCAA",
+                  "CCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAAACCCTAAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCCCAACCCTAACCCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCC");
+    wfagpu_add_sequences(&aligner,
+                         "TAACCCTAACCCTAACCCTAACCCTACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAACCTAACCCTACCCTAACCCAAACCCTAACCCCAACCCCAACCCCAACCC",
+                         "TAACCCTACCCTAACCCTAACCCTACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTTAACCCTAACCCTTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCC");
+    wfagpu_add_sequences(&aligner,
+                         "GGTGAGGGTGAGGGTTAGGGTTAGGGTGAGGGTTAGGGTGAGGGTGAGGGTGAGGGTGAGGGTAGGGGTAGGGGGTAGGGGTGGGGGTGGGTGAGGGGTAGGGGTAGGGAGAGGGGTAGGGTTAGGGGTGGGGGGAGGGTGAGGGTTA",
+                         "GGTGAGGGTGAGGGTTAGGGTTAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTGAGGGTTAGGGTGTTAGAGGGTTAGGGTTAGGGTTAGGGTTAGGGTTAGGGGGTTAGGGGGTTAGGGGGTTAGGGGGTTAGGGTGAGGGTGAGGGT");
+    wfagpu_add_sequences(&aligner,
+                         "CCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCTAACCCTCACCTAACCCCAACCCTAACCCCAACCCCAACCCACACCCTAC",
+                         "CCTAACCCTAACCCTAACCCTAACCCTAACCCCTAACCCTAACCCTAAACCCTAAACCCTAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCCCAACCCTAACCCCTAACCCTAACCCTAACCCTAC");
 
-    for (int i=0; i<num_alignments; i++) {
-        printf("Alignment %d:\n", i);
-        printf("\tQuery (offset=%zu, len=%d): %s\n", sequences_metadata[i].pattern_offset, sequences_metadata[i].pattern_len, &sequences_buffer[sequences_metadata[i].pattern_offset]);
-        printf("\tText (offset=%zu, len=%d): %s\n", sequences_metadata[i].text_offset, sequences_metadata[i].text_len, &sequences_buffer[sequences_metadata[i].text_offset]);
-    }
-    printf("--------------------------------------------------------------------------------\n");
+    // Initialize the alignment parameters (*after* the sequences have been
+    // added)
+    affine_penalties_t penalties = {.x = 2, .o = 3, .e = 1};
+    wfagpu_initialize_parameters(&aligner, penalties);
 
-    // Initialize structures to store the results (CIGARS + scores)
-    wfa_alignment_result_t* results;
-    // Assign an initial CIGAR length (in bytes). The CIGARs buffers are resized
-    // if they need more space, but having an adecuate initial CIGAR size can
-    // improve performance.
-    const int cigar_len = 50;
-    if (!initialize_wfa_results(&results, num_alignments, cigar_len)) {
-        fprintf(stderr, "Can not initialize CIGAR buffer.");
-        exit(-1);
-    }
+    // Optionally set batch size, in this case (we have 4 alginments), setting a
+    // batch size of 2 will execute two batches
+    wfagpu_set_batch_size(&aligner, 2);
 
-    // Initialize alignment options
-    wfa_alignment_options_t wfa_options = {0};
+    // With wfagpu_initialize_parameters all the alignment parameters are set to
+    // its default, but it is possible to change them to obtain a better
+    // performance depending on the dataset.
 
     // Maximum error (score) supported by the kernel. This is a very important
     // parameter to achieve good performance. Try to keep this parameter as low
     // as possible. It is better to set it for majority of the dataset, letting
     // the corner cases with higher error to be offloaded to the CPU.
-    const size_t max_error = 100;
-    wfa_options.max_error = max_error;
+    aligner.alignment_options.max_error = 100;
 
     // Number of CUDA threads per alignment. Some performance considerations:
     // - It needs to be a multiple of the warp size (i.e. multiple of 32)
@@ -145,42 +72,26 @@ int main() {
     //   It is a good idea not to give the maximum number of thread per
     //   alignment, as some of those threads will never be used until the last
     //   iterations.
-    //const int threads_per_block = get_threads_per_alignment(max_error);
-    //wfa_options.threads_per_block = threads_per_block;
-    wfa_options.threads_per_block = 128;
+    aligner.alignment_options.threads_per_block = 128;
 
     // Number of GPU "workers" (alignmers). This parameter depends on the GPU
-    // architecture, and the number of threads.
-    wfa_options.num_workers = get_num_workers(wfa_options.threads_per_block);
+    // architecture, and the number of threads. Only set this if you really
+    // understand what you are doing.
+    aligner.alignment_options.num_workers = get_num_workers(aligner.alignment_options.threads_per_block);
 
     // Set to BAND_NONE to not use adaptative band and get exact alignments.
     // Otherwise, set the maximum diagonal that will be initially computed.
-    wfa_options.band = BAND_NONE;
+    aligner.alignment_options.band = BAND_NONE;
 
-
-    wfa_options.batch_size = batch_size;
-    wfa_options.num_alignments = num_alignments;
-
-    // Penalties need to be positive, match score is always 0
-    affine_penalties_t penalties = {.x = 2, .o = 3, .e = 1};
-    wfa_options.penalties = penalties;
-
-    launch_alignments(
-        sequences_buffer,
-        sequences_buffer_size,
-        sequences_metadata,
-        results,
-        wfa_options,
-        false // Check if results are correct
-    );
+    // Align all sequence pairs
+    wfagpu_align(&aligner);
 
     // Read the results
-    for (int i=0; i<wfa_options.num_alignments; i++) {
+    for (int i=0; i<aligner.alignment_options.num_alignments; i++) {
         printf("Alignment %d:\n", i);
-        printf("\tScore: %d\n", results[i].error);
-        printf("\tCIGAR: %s\n", results[i].cigar.buffer);
+        printf("\tScore: %d\n", aligner.results[i].error);
+        printf("\tCIGAR: %s\n", aligner.results[i].cigar.buffer);
     }
-
-    free(sequences_buffer);
-    free(sequences_metadata);
+    // Free internal structures memory
+    wfagpu_destroy_aligner(&aligner);
 }
